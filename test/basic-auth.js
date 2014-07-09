bf4d8b05c3c5d62c15b0c101bc9bf6145fff8648
Troy Goode <troygoode@gmail.com>
2014-07-08 20:46:19 -0700

    testing for #25

---
+++ test/basic-auth.js
@@ -0,0 +1,45 @@
+/*jslint indent: 2*/
+/*global require: true, module: true, describe: true, it: true, setTimeout: true*/
+
+(function () {
+
+  'use strict';
+
+  var should = require('should'),
+    express = require('express'),
+    supertest = require('supertest'),
+    basicAuth = require('basic-auth-connect'),
+    cors = require('../lib'),
+    app;
+
+  /* -------------------------------------------------------------------------- */
+
+  app = express();
+  app.use(basicAuth('username', 'password'));
+  app.use(cors());
+  /*jslint unparam: true*/ // `req` is part of the signature, but not used in these routes
+  app.post('/', function (req, res) {
+    res.send('hello world');
+  });
+  /*jslint unparam: false*/
+
+  /* -------------------------------------------------------------------------- */
+
+  describe('basic auth', function () {
+    describe('', function () {
+      it('POST works', function (done) {
+        supertest(app)
+          .post('/')
+          .auth('username', 'password')
+          .expect(200)
+          .end(function (err, res) {
+            should.not.exist(err);
+            res.text.should.eql('hello world');
+            done();
+          });
+      });
+    });
+  });
+
+}());
+
