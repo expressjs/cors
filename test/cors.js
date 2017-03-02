d88db6ebf804391b438bc3e153b77e838b8c4a47
Tomek Kopczuk <tkopczuk@gmail.com>
2017-03-02 09:16:55 +0100

    Sends “Vary: Origin” header when Origin is not whitelisted to prevent caching the failed request.

--- test/cors.js
+++ test/cors.js
@@ -238,7 +238,7 @@
         cors(options)(req, res, function(err) {
           should.not.exist(err);
           should.not.exist(res.getHeader('Access-Control-Allow-Origin'));
-          should.not.exist(res.getHeader('Vary'));
+          should.exist(res.getHeader('Vary'));
           return done();
         });
       });
